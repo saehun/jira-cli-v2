323a8a980d7883020cec47282ab4d3c6fe08efbd
minidonut <nycom13@gmail.com>
2020-09-07 01:36:57 +0900

    add date-fn format

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -1,13 +1,18 @@
 import * as fs from 'fs-extra';
 import { Issue } from './model';
+import { formatRelative } from 'date-fns';
+import { ko } from 'date-fns/locale';
 import { JIRA_ISSUE_INDEX_PATH } from './env';
 
 function stringifyIssue(issue: Issue): string {
-  console.log(issue);
+  const date = formatRelative(new Date(issue.fields.updated), new Date(), {
+    locale: ko,
+  });
   // prettier-ignore
-  return `${issue.fields.status.name}`.padEnd(10, ' ') +
+  return `${date}`.padEnd(10, ' ') +
+         `${issue.fields.status.name}`.padEnd(10, ' ') +
          `[${issue.key}]`.padEnd(13, ' ') +
-         `${issue.fields.summary}`;
+         `${issue.fields.summary}`
 }
 
 export async function saveToFile(issues: Issue[]): Promise<void> {
