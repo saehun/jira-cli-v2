1877d3d303be1091ccb533f0ce282b653d079520
minidonut <nycom13@gmail.com>
2020-09-06 23:58:05 +0900

    extract io function to io.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
-import * as fs from 'fs-extra';
 import { peco } from './peco';
 import { JIRA_ISSUE_INDEX_PATH } from './env';
-import { Issue } from './model';
-
-function stringifyIssue(issue: Issue): string {
-  console.log(issue);
-  // prettier-ignore
-  return `${issue.fields.status.name}`.padEnd(10, ' ') +
-         `[${issue.key}]`.padEnd(13, ' ') +
-         `${issue.fields.summary}`;
-}
-
-async function saveToFile(issues: Issue[]): Promise<void> {
-  const index = issues.map(stringifyIssue).join('\n');
-  console.log(index);
-  await fs.outputFile(JIRA_ISSUE_INDEX_PATH, index);
-  return;
-}
 
 async function main(): Promise<void> {
   // const { issues } = await getIssues();
---
+++ src/io.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs-extra';
+import { Issue } from './model';
+import { JIRA_ISSUE_INDEX_PATH } from './env';
+
+export function stringifyIssue(issue: Issue): string {
+  console.log(issue);
+  // prettier-ignore
+  return `${issue.fields.status.name}`.padEnd(10, ' ') +
+         `[${issue.key}]`.padEnd(13, ' ') +
+         `${issue.fields.summary}`;
+}
+
+export async function saveToFile(issues: Issue[]): Promise<void> {
+  const index = issues.map(stringifyIssue).join('\n');
+  console.log(index);
+  await fs.outputFile(JIRA_ISSUE_INDEX_PATH, index);
+  return;
+}
