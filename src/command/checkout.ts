dfbc959bf284be9c86537536481412473ccd9a47
minidonut <nycom13@gmail.com>
2020-09-07 02:07:39 +0900

    implement help

--- src/command/checkout.ts
+++ src/command/checkout.ts
@@ -1,5 +1,31 @@
+import * as execa from 'execa';
+import { peco } from '../lib/peco';
+
 async function command(): Promise<void> {
-  console.log('checkout 😎');
+  try {
+    const { stdout } = await execa('git', ['status']);
+    if (!stdout.includes('working tree clean')) {
+      console.log(stdout);
+      console.log(`
+commit or stash to clear working tree first 😔
+`);
+      process.exit(0);
+    }
+  } catch (e) {
+    console.log(e.stderr);
+    process.exit(0);
+  }
+
+  const { key } = await peco();
+  try {
+    await execa('git', ['checkout', '-b', key]);
+  } catch (e) {
+    if (e.stderr.includes('already exists')) {
+      await execa('git', ['checkout', key]);
+    } else {
+      console.log(e.stderr);
+    }
+  }
 }
 
 export default command;
