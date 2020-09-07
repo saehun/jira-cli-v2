722d08893063c67d56df340cb61235bb88a0bd8c
minidonut <nycom13@gmail.com>
2020-09-07 16:40:56 +0900

    reduce loading time

--- src/command/list.ts
+++ src/command/list.ts
@@ -1,8 +1,7 @@
 import { peco } from '../lib/peco';
-import { printIssue } from '../lib/print';
 async function command(): Promise<void> {
   const { key } = await peco('open');
-  await printIssue(key);
+  require('../lib/print').printIssue(key);
 }
 
 export default command;
