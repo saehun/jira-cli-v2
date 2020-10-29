306bc7a15c935bba2a5b4387fb505c1e8ca96360
minidonut <nycom13@gmail.com>
2020-10-29 16:04:36 +0900

    Dont' save index file

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -1,20 +1,8 @@
 import * as fs from 'fs-extra';
 import { Issue } from './model';
-import { formatRelative } from './formatRelative';
 import { JIRA_ISSUE_INDEX_PATH } from './env';
 
-function stringifyIssue(issue: Issue): string {
-  const date = formatRelative(issue.fields.updated);
-  // prettier-ignore
-  return `${date}`.padEnd(13, ' ') +
-         `${issue.fields.status.name}`.padEnd(10, ' ') +
-         `[${issue.key}]`.padEnd(13, ' ') +
-         `${issue.fields.summary}`
-}
-
 export async function saveToFile(issues: Issue[]): Promise<void> {
-  const index = issues.map(stringifyIssue).join('\n');
-  await fs.outputFile(JIRA_ISSUE_INDEX_PATH, index);
   await fs.outputFile(JIRA_ISSUE_INDEX_PATH + '.json', JSON.stringify(issues));
   return;
 }
