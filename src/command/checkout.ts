ed3407f9103e8bd08909eaf8d89a0e21773b9d54
minidonut <nycom13@gmail.com>
2021-04-05 00:05:06 +0900

    Can checkout with prefix

--- src/command/checkout.ts
+++ src/command/checkout.ts
@@ -12,13 +12,21 @@ async function command(): Promise<void> {
 
   const key = await select(`checkout from ${current}`);
   try {
-    await execa('git', ['checkout', '-b', key]);
+    await execa('git', ['checkout', '-b', generateBranchName(key)]);
   } catch (e) {
     if (e.stderr.includes('already exists')) {
       await execa('git', ['checkout', key]);
     } else {
       console.log(e.stderr);
     }
+  }
+
+  function generateBranchName(key: string): string {
+    const prefix = process.argv[3];
+    if (prefix === 'dev') {
+      return `dev/${key}`;
+    }
+    return key;
   }
 }
 
